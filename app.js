48c86f05b92893385c9fff69b83d696f611ddbdd
liliping.fe <liliping.fe@bytedance.com>
2019-12-12 10:20:46 +0800

    feat: 优化缓存已下载的图片

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const fs = require('fs');
 const axios = require('axios');
-const { JSDOM } = require("jsdom");
-const moment = require("moment");
-const rimraf = require("rimraf");
+const { JSDOM } = require('jsdom');
+const { ncp } = require('ncp');
+const { noop } = require('lodash');
+const moment = require('moment');
+const rimraf = require('rimraf');
 
 const types = ['cat', 'dog', 'avengers', 'cute', 'tom+and+jerry',
   'Alvin+and+the+Chipmunks', 'minions'];
-const dist = './download';
-const archive = './archive';
+const downloadDir = './download';
+const archiveDir = './archive';
 const domain = 'https://wall.alphacoders.com';
 
 const  hour = 1000 * 60 * 60; // 一个小时
 const interval = hour * 2;
 
 const log = console.log;
-console.log = function(...args) {
+console.log = function (...args) {
   log.call(console, getTime(), '---', ...args);
-}
+};
 
 // https://wall.alphacoders.com/by_category.php?id=9&name=Dark+Wallpapers
 function getApiUrl(type, page = 1) {
-  return `${domain}/search.php?search=${type}&page=${page}`
+  return `${domain}/search.php?search=${type}&page=${page}`;
 }
 
 function getTime() {
   return moment().format('YYYY-MM-DD HH.mm');
 }
-
-let folderName = getTime();
 
 async function download(url, i) {
   const fileName = getTime() + '---' + i;
@@ -35,10 +35,10 @@ async function download(url, i) {
   await axios({
     method: 'get',
     url,
-    responseType: 'stream'
-  }).then(function(response) {
-    response.data.pipe(fs.createWriteStream(`${dist}/${fileName}.jpeg`))
-  })
+    responseType: 'stream',
+  }).then(function (response) {
+    response.data.pipe(fs.createWriteStream(`${downloadDir}/${fileName}.jpeg`));
+  });
 }
 
 async function curl(url) {
@@ -64,11 +64,14 @@ function findAllUrls(htmlText, imgSelector) {
   return res;
 }
 
-function getRandom(type) {
-  const length = type.length;
+/**
+ * 从数组中获取随机值
+ */
+function getRandom(arr) {
+  const length = arr.length;
   const random = Math.floor(Math.random() * length);
 
-  return type[random];
+  return arr[random];
 }
 
 function getPageNumber(htmlText) {
@@ -82,37 +85,73 @@ function getPageNumber(htmlText) {
   return random;
 }
 
+/** 缓存中获取图片 */
+function copyRandomCache() {
+  const cacheDir = fs.readdirSync(archiveDir);
+  const randomCacheDirName = getRandom(cacheDir).replace(/\s/, '\ ');
+  ncp(`${archiveDir}/${randomCacheDirName}`, downloadDir, noop);
+  console.log('获取缓存图片', `${archiveDir}/${randomCacheDirName}`);
+}
+
+// 用于记录上一次的类型和页数
+let folderName = '';
+
 async function main() {
-  const type = getRandom(types);
+  try {
+    const type = getRandom(types);
 
-  // 先 curl 获取 page
-  let url = getApiUrl(type);
-  let htmlText = await curl(url);
-  const page = getPageNumber(htmlText);
+    // 先 curl 获取 page
+    let url = getApiUrl(type);
+    let htmlText = await curl(url);
+    const page = getPageNumber(htmlText);
 
-  // 真正 curl 的地址
-  url = getApiUrl(type, page);
-  htmlText = await curl(url);
+    // 真正 curl 的地址
+    url = getApiUrl(type, page);
+    console.log('真正的curl地址', url);
+    const tmpFolder = `${type}${page}`;
 
-  console.log('真正的curl地址', url);
+    if (folderName && fs.existsSync(`${archiveDir}/${tmpFolder}`)) {
+      if (Math.random() < 0.3) {
+        return main();
+      } else {
+        copyRandomCache();
+      }
+    }
 
-  const imgSelector = '.thumb-container .boxgrid a';
-  const urls = findAllUrls(htmlText, imgSelector);
+    if(!fs.existsSync(downloadDir)) {
+      console.log('mkdir', downloadDir);
+      fs.mkdirSync(downloadDir);
+    }
 
-  // rimraf.sync(`${dist}/*`);
+    // 如果 download 文件夹有文件，就移动一下
+    let downloadFiles = fs.readdirSync(downloadDir);
+    downloadFiles = downloadFiles.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
 
-  if (fs.statSync(dist).isDirectory()) {
-    fs.renameSync(dist, `${archive}/${folderName}`);
-    fs.mkdirSync(dist);
-  }
+    if (folderName && downloadFiles.length) {
+      fs.renameSync(downloadDir, `${archiveDir}/${folderName}`);
+    } else {
+      rimraf.sync(downloadDir);
+    }
 
-  folderName = getTime();
+    fs.mkdirSync(downloadDir);
 
-  for (let i = 0, len = urls.length; i < len; i++) {
-    const realImgHtmlText = await curl(urls[i]);
-    const realImg = findAllUrls(realImgHtmlText, '.main-content');
+    folderName = tmpFolder;
+
+    htmlText = await curl(url);
+
+    const imgSelector = '.thumb-container .boxgrid a';
+    const urls = findAllUrls(htmlText, imgSelector);
+
+    for (let i = 0, len = urls.length; i < len; i++) {
+      const realImgHtmlText = await curl(urls[i]);
+      const realImg = findAllUrls(realImgHtmlText, '.main-content');
+
+      await download(realImg[0], i);
+    }
     
-    await download(realImg[0], i);
+  } catch (e) {
+    console.log('main 函数报错: ', e);
+    copyRandomCache();
   }
 }
 
